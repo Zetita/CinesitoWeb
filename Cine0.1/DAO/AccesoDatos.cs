d614da773f46639ff48a7d7c95d9b721ef6be8c0
Sheila Abril Diz <sheila.diz@alumnos.frgp.utn.edu.ar>
2019-06-01 17:59:38 -0300

    Agregado de acceso de datos

--- Cine0.1/DAO/AccesoDatos.cs
+++ Cine0.1/DAO/AccesoDatos.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DAO
 {
     public class AccesoDatos
     {
+        string rutaBase = "Data Source=localhost\\sqlexpress;Initial Catalog=CineFrenz;Integrated Security=True";
+
+        public AccesoDatos()
+        {
+
+        }
+
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection cn = new SqlConnection(rutaBase);
+            try
+            {
+                cn.Open();
+                return cn;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public SqlDataAdapter ObtenerAdaptador(String consultaSql)
+        {
+            SqlDataAdapter adaptador;
+            try
+            {
+                adaptador = new SqlDataAdapter(consultaSql, rutaBase);
+                return adaptador;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
     }
 }
