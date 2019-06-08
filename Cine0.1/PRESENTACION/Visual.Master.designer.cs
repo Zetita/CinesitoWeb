f53012506ef3fdb1df41dfaa3c4552595e4608fc
Sheila Abril Diz <sheila.diz@alumnos.frgp.utn.edu.ar>
2019-06-08 18:38:19 -0300

    Cambios en codigo de Visual.Master y de EstiloPag.css. Uso de cssClass para no repetir codigo de estilo

--- Cine0.1/PRESENTACION/Visual.Master.designer.cs
+++ Cine0.1/PRESENTACION/Visual.Master.designer.cs
@@ -67,6 +67,15 @@ namespace PRESENTACION {
         protected global::System.Web.UI.WebControls.Button Button3;
         
         /// <summary>
+        /// Control ImageButton3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton3;
+        
+        /// <summary>
         /// Control ImageButton2.
         /// </summary>
         /// <remarks>
