0940455fd8df292ddee36c0182b23d1859c0d5df
Axel De La Fuente <dodge_challenger@hotmail.com.ar>
2019-06-23 17:18:01 -0300

    Codigo de Butacas optimizado para que en vez de ocupar 300 lineas ocupe 110.

--- Cine0.1/PRESENTACION/Butacas.aspx.cs
+++ Cine0.1/PRESENTACION/Butacas.aspx.cs
@@ -18,12 +18,12 @@ namespace PRESENTACION
         {
             DAO_BxF BxF = new DAO_BxF();
             DAO_Funciones Funcion = new DAO_Funciones();
-            string ID_Funcion = Application["ID_Funcion"].ToString();
-            //string ID_Funcion = "1";
+            //string ID_Funcion = Application["ID_Funcion"].ToString();
+            string ID_Funcion = "1";
             string Consulta = "Select * from ButacaxFunciones where ID_Funcion=" + ID_Funcion;
             DataTable dt = BxF.ObtenerTablaBxF(Consulta);
             if(!IsPostBack) Application["CantEntradasT"] = Application["CantEntradas"] = 10;
-
+            string NombreBoton;
 
             for (int x = 1; x <= 44; x++)
             {
@@ -31,36 +31,16 @@ namespace PRESENTACION
                 {
                     if (x.ToString() == dt.Rows[j]["ID_Butaca"].ToString().Trim())
                     {
-                        Colorear(x.ToString());
+                        NombreBoton = "btn" + x.ToString();
+                        Colorear(Page, NombreBoton);
                     }
                 }
+                
             }
 
             Consulta = ArmarConsultaHeavy(ID_Funcion);
             dt = Funcion.ObtenerTablaFunciones(Consulta);
             LlenarResumen(dt);
-            
-        }
-
-        protected void Colorear2(object sender, EventArgs e)
-        {
-            Button Clickeado = (Button)sender;
-            if (Clickeado.BackColor == Color.Green)
-            {
-                if (Convert.ToInt32(Application["CantEntradasT"].ToString()) < 10)
-                {
-                    Clickeado.BackColor = Color.Empty;
-                    Application["CantEntradasT"] = Convert.ToInt32(Application["CantEntradasT"]) + 1;
-                }
-            }
-            else
-            {
-                if (Convert.ToInt32(Application["CantEntradasT"].ToString()) > 0)
-                {
-                    Clickeado.BackColor = Color.Green;
-                    Application["CantEntradasT"] = Convert.ToInt32(Application["CantEntradasT"]) - 1;
-                }
-            }
         }
 
         protected void btnVolver_Click(object sender, EventArgs e)
@@ -102,190 +82,43 @@ namespace PRESENTACION
             return Consulta;
         }
 
-        ///Perdon Sheila, era necesario :(
-
-        protected void Colorear(string x)
+        public static void Colorear(Page Pagina, string Nombre)
         {
-            switch (x)
+            Button Boton;
+            foreach (Control ctrl in Pagina.Form.Controls)
             {
-                case "1":
-                        btn1.BackColor = Color.Red;
-                        btn1.Enabled = false;
-                    break;
-                case "2":
-                        btn2.BackColor = Color.Red;
-                        btn2.Enabled = false;
-                    break;
-                case "3":
-                        btn3.BackColor = Color.Red;
-                        btn3.Enabled = false;
-                    break;
-                case "4":
-                        btn4.BackColor = Color.Red;
-                        btn4.Enabled = false;
-                    break;
-                case "5":
-                        btn5.BackColor = Color.Red;
-                        btn5.Enabled = false;
-                    break;
-                case "6":
-                        btn6.BackColor = Color.Red;
-                        btn6.Enabled = false;
-                    break;
-                case "7":
-                        btn7.BackColor = Color.Red;
-                        btn7.Enabled = false;
-                    break;
-                case "8":
-                        btn8.BackColor = Color.Red;
-                        btn8.Enabled = false;
-                    break;
-                case "9":
-                        btn9.BackColor = Color.Red;
-                        btn9.Enabled = false;
-                    break;
-                case "10":
-                        btn10.BackColor = Color.Red;
-                        btn10.Enabled = false;
-                    break;
-                case "11":
-                        btn11.BackColor = Color.Red;
-                        btn11.Enabled = false;
-                    break;
-                case "12":
-                        btn12.BackColor = Color.Red;
-                        btn12.Enabled = false;
-                    break;
-                case "13":
-                        btn13.BackColor = Color.Red;
-                        btn13.Enabled = false;
-                    break;
-                case "14":
-                        btn14.BackColor = Color.Red;
-                        btn14.Enabled = false;
-                    break;
-                case "15":
-                        btn15.BackColor = Color.Red;
-                        btn15.Enabled = false;
-                    break;
-                case "16":
-                        btn16.BackColor = Color.Red;
-                        btn16.Enabled = false;
-                    break;
-                case "17":
-                        btn17.BackColor = Color.Red;
-                        btn17.Enabled = false;
-                    break;
-                case "18":
-                        btn18.BackColor = Color.Red;
-                        btn18.Enabled = false;
-                    break;
-                case "19":
-                        btn19.BackColor = Color.Red;
-                        btn19.Enabled = false;
-                    break;
-                case "20":
-                        btn20.BackColor = Color.Red;
-                        btn20.Enabled = false;
-                    break;
-                case "21":
-                        btn21.BackColor = Color.Red;
-                        btn21.Enabled = false;
-                    break;
-                case "22":
-                        btn22.BackColor = Color.Red;
-                        btn22.Enabled = false;
-                    break;
-                case "23":
-                        btn23.BackColor = Color.Red;
-                        btn23.Enabled = false;
-                    break;
-                case "24":
-                        btn24.BackColor = Color.Red;
-                        btn24.Enabled = false;
-                    break;
-                case "25":
-                        btn25.BackColor = Color.Red;
-                        btn25.Enabled = false;
-                    break;
-                case "26":
-                        btn26.BackColor = Color.Red;
-                        btn26.Enabled = false;
-                    break;
-                case "27":
-                        btn27.BackColor = Color.Red;
-                        btn27.Enabled = false;
-                    break;
-                case "28":
-                        btn28.BackColor = Color.Red;
-                        btn28.Enabled = false;
-                    break;
-                case "29":
-                        btn29.BackColor = Color.Red;
-                        btn29.Enabled = false;
-                    break;
-                case "30":
-                        btn30.BackColor = Color.Red;
-                        btn30.Enabled = false;
-                    break;
-                case "31":
-                        btn31.BackColor = Color.Red;
-                        btn31.Enabled = false;
-                    break;
-                case "32":
-                        btn32.BackColor = Color.Red;
-                        btn32.Enabled = false;
-                    break;
-                case "33":
-                        btn33.BackColor = Color.Red;
-                        btn33.Enabled = false;
-                    break;
-                case "34":
-                        btn34.BackColor = Color.Red;
-                        btn34.Enabled = false;
-                    break;
-                case "35":
-                        btn35.BackColor = Color.Red;
-                        btn35.Enabled = false;
-                    break;
-                case "36":
-                        btn36.BackColor = Color.Red;
-                        btn36.Enabled = false;
-                    break;
-                case "37":
-                        btn37.BackColor = Color.Red;
-                        btn37.Enabled = false;
-                    break;
-                case "38":
-                        btn38.BackColor = Color.Red;
-                        btn38.Enabled = false;
-                    break;
-                case "39":
-                        btn39.BackColor = Color.Red;
-                        btn39.Enabled = false;
-                    break;
-                case "40":
-                        btn40.BackColor = Color.Red;
-                        btn40.Enabled = false;
-                    break;
-                case "41":
-                        btn41.BackColor = Color.Red;
-                        btn41.Enabled = false;
-                    break;
-                case "42":
-                        btn42.BackColor = Color.Red;
-                        btn42.Enabled = false;
-                    break;
-                case "43":
-                        btn43.BackColor = Color.Red;
-                        btn43.Enabled = false;
-                    break;
-                case "44":
-                        btn44.BackColor = Color.Red;
-                        btn44.Enabled = false;
-                    break;
+                foreach (Control Control in ctrl.Controls)
+                    if (Control is Button)
+                    {
+                        Boton = (Button)Control;
+                        if (Boton.ID == Nombre)
+                        {
+                            Boton.BackColor = Color.Red;
+                        }
+                    }
             }
         }
-        
+
+        protected void Colorear2(object sender, EventArgs e)
+        {
+            Button Clickeado = (Button)sender;
+            if (Clickeado.BackColor == Color.Green)
+            {
+                if (Convert.ToInt32(Application["CantEntradasT"].ToString()) < 10)
+                {
+                    Clickeado.BackColor = Color.Empty;
+                    Application["CantEntradasT"] = Convert.ToInt32(Application["CantEntradasT"]) + 1;
+                }
+            }
+            else
+            {
+                if (Convert.ToInt32(Application["CantEntradasT"].ToString()) > 0)
+                {
+                    Clickeado.BackColor = Color.Green;
+                    Application["CantEntradasT"] = Convert.ToInt32(Application["CantEntradasT"]) - 1;
+                }
+            }
+        }
+
     }
 }
