625bbca0be3e47c9e2df0b1f13a15ede88d3858e
AxelNegro <51132582+AxelNegro@users.noreply.github.com>
2019-06-24 22:14:29 -0300

    Buscador JS

---
+++ Cine0.1/PRESENTACION/JS/Buscador.js
@@ -0,0 +1,18 @@
+﻿var db = [];
+function onChange(e, value) {
+    // get animation object
+    var searchBtn = $(this).jellyboSearchButton003();
+    db.push(value);
+    if (db.length > 3) {
+        db.shift();
+    }
+    if (value.length < 1) {
+        db = [];
+    }
+    //first empty suggestion list
+    searchBtn.emptyList();
+    for (var i in db) {
+        // added new suggestions
+        searchBtn.addListItem(db[i]);
+    }
+}
